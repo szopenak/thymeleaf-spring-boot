4b939e72df8a50f55607a80bc52653ffe3f3299d
Artur Krotiuk <arturkrotiuk@gmail.com>
2018-06-02 21:45:34 +0200

    Added taking books

--- src/main/java/com/akrot/bookcrossing/controllers/BookController.java
+++ src/main/java/com/akrot/bookcrossing/controllers/BookController.java
@@ -3,18 +3,17 @@ package com.akrot.bookcrossing.controllers;
 import com.akrot.bookcrossing.model.Book;
 import com.akrot.bookcrossing.model.BookDto;
 import com.akrot.bookcrossing.repository.BookRepository;
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.RegularExpression;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.Map;
+import java.util.Optional;
 
 @Controller
 @RequestMapping
@@ -24,7 +23,16 @@ public class BookController
     BookRepository bookRepository;
 
     @GetMapping("/books")
-    public ModelAndView welcome() {
+    public ModelAndView welcome(@RequestParam(required = false) String take) {
+        if (take != null && take.matches("[0-9]*")){
+            Long id = Long.valueOf(take);
+            Optional<Book> toGet = bookRepository.findById(id);
+            if (toGet.isPresent()) {
+                Book book = toGet.get();
+                book.setAvailable(false);
+                bookRepository.save(book);
+            }
+        }
         return new ModelAndView("books").addObject("books", bookRepository.getAllFree());
     }
 
